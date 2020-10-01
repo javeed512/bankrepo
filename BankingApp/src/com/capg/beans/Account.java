7a05a8499b4311e677acd62c08b0ab491dc0a9e8
javeedmohammedjaveed@gmail.com <javeedmohammedjaveed@gmail.com>
2020-10-01 18:19:30 +0530

    adding bank app by javeed

---
+++ BankingApp/src/com/capg/beans/Account.java
@@ -0,0 +1,23 @@
+package com.capg.beans;
+
+public class Account {
+
+	
+	private int accountNum;
+	private  String accountType;
+	public int getAccountNum() {
+		return accountNum;
+	}
+	public void setAccountNum(int accountNum) {
+		this.accountNum = accountNum;
+	}
+	public String getAccountType() {
+		return accountType;
+	}
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+	
+	
+	
+}
---
+++ BankingApp/src/com/capg/client/User.java
@@ -0,0 +1,21 @@
+package com.capg.client;
+
+import com.capg.beans.Account;
+
+public class User {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Account ac = new Account();
+		
+		
+		ac.setAccountNum(9900);
+		ac.setAccountType("Savings");
+		
+		
+		System.out.println(ac);
+		
+	}
+
+}
